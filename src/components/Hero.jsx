a1090816d2f2af00c82e78627a58ae18e21033e2
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-10-26 22:04:10 +0500

    navbar completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className=''>
+        <div className="max-w-800px">
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
-const navbar = () => {
-  return (
+const Navbar = () => {
+
+  const[ nav , setNAv ] = useState(false)
+  const handleNav = () => { 
+          setNAv(!nav)
+    }
+   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'> 
       <h1 className='w-full text-3xl font-bold text-[#000000]'>NavBar</h1>
 
-      <ul className='flex'>
+      <ul className='hidden lg:flex'>
         <li className='p-2'>Home</li>
         <li className='p-2'>Company</li>
         <li className='p-2'>Resources</li>
         <li className='p-2'>About</li>
+        <li className="p-2">contact</li>
         
 
       </ul>
-       <div>
-          <AiOutlineMenu size={20}/>
+       <div onClick={handleNav} className="block lg:hidden">
+          {!nav  ? <AiOutlineMenu size={20}/> : <AiOutlineClose  size={20} /> }
+       </div>
+       <div  className={`fixed left-0 top-0 w-[40%] border-r border-r-aquamarine bg-[white] h-full ease-in-out duration-300 
+        ${
+    nav ? "translate-x-0 " : "translate-x-[-100%]"
+        }`}>
+   <h1 className='w-full text-3xl font-bold text-[#000000] m-4'>NavBar</h1>
+          <ul className='uppercase p-4'>
+            <li className='p-2 border-b'>Home</li>
+            <li className='p-2 border-b'>Company</li>
+            <li className='p-2 border-b'>Resources</li>
+            <li className='p-2 border-b'>About</li>
+            <li className='p-2'>contact</li>
+
+          </ul>
        </div>
     </div>
     
   )
 }
 
-export default navbar
+export default Navbar
