e6ebf853514092cbf842b99d2d079085b8c40ce4
zalmi <78726819+zalmicoding@users.noreply.github.com>
2022-11-16 12:26:11 +0500

    newsslatter added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="max-w-800px mt-[-140px] w-full h-screen mx-auto text-center flex flex-col   justify-center">
             <p className='lg:text-3xl sm:text-4xl text:xl font-bold  p-2'>software company</p>
 
-            <h6 className='text-[#b2c08d] lg:text-4xl sm:text-6xl text:2xl font-bold md:py-6'>phi horizon</h6>
+            <h6 className='text-[#b2c08d] lg:text-4xl sm:text-6xl text:2xl font-bold md:py-6'>worldwebtree</h6>
 
             <div className='flex justify-center items-center'>
 
@@ -25,7 +25,7 @@ const Hero = () => {
                     
             </div>  
         
-            <p className='lg:text-2xl sm:4xl  lg:pl-4 text:lg  mt-4 font-bold pl-2 text-[#55dafceb]'>phi horizon is a company where they provide sercices about software </p>
+            <p className='lg:text-2xl sm:4xl  lg:pl-4 text:lg  mt-4 font-bold pl-2 text-[#55dafceb]'>worldwebtree is a company where they provide sercices about software </p>
             <button className='bg-[black] text-[#50d150]  w-[200px] rounded-md font-medium my-6 mx-auto py-3'>Get Started</button>
            
 
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,8 +5,15 @@ const Newsletter = () => {
    <div className='w-full '>
         <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
             <div>
-                <h1 className='lg:text-3xl sm:text-4xl'>this is phi horizon company </h1>
+                <h1 className='lg:text-3xl sm:text-4xl'>this is company </h1>
                 <p> SignUp for newsletter and stay up to date </p>
+            </div>
+
+            <div className='my-4'>
+               <div>
+                  <input type="email" placeholder='Email'/>
+                  <button className='bg-[black] text-[#50d150]  w-[100px] rounded-md font-medium my-6 mx-auto py-3'>Notify me</button>
+               </div>
             </div>
         </div>
     </div>
